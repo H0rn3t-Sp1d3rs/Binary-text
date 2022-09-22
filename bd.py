5e9a8bba2ded9baabf9f8a657505afe5add2e573
H0rn3t Sp1d3rs <97798085+H0rn3t-Sp1d3rs@users.noreply.github.com>
2022-09-23 01:08:01 +0600

    Update bd.py

--- bd.py
+++ bd.py
@@ -1,7 +1,6 @@
 import binascii
+print("\033[1;32;40mEnter Your Text")
+B = raw_input("=> \033[1;31;40m")
 
-print("\n\033[1;32;40mEnter Your Text : ")
-angga = raw_input("=> ")
-
-print("\n\033[1;36;40mYour Bin Code => ")
-print(' '.join(format(ord(x), 'b') for x in angga))
+print("\n\033[1;36;40mYour Bin Code => \033[1;31;40m")
+print(' '.join(format(ord(x), 'b') for x in B))
